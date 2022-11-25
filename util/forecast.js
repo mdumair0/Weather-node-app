3864a92869bdaf3c8b3996abbcf9e7745dffe585
unknown <mohd.umair2325@gmail.com>
2022-11-25 22:49:33 +0530

    Added some features

--- util/forecast.js
+++ util/forecast.js
@@ -1,7 +1,7 @@
 const req = require('request')
 
 const forecast=(latitude,longitude,location,cb)=>{
-    const url = "http://api.weatherstack.com/current?access_key=e1b8480d0f5a7c7d0f65b6506b03ddab&query="+latitude+','+longitude;
+    const url = "http://api.weatherstack.com/current?access_key=c627de678526c81207f083c89e2f52be&query="+latitude+','+longitude;
     
     req({ url, json: true }, (error, { body }) => {
         if (error) {
