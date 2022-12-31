6af7edfe1d15a96584b974cd264a1f274ff08557
unknown <mohd.umair2325@gmail.com>
2022-12-31 21:10:01 +0530

    Contact added

--- public/js/contact.js
+++ public/js/contact.js
@@ -3,17 +3,23 @@ const contactName = document.querySelector('#fname');
 const contactEmail = document.querySelector('#email');
 const contactSubject = document.querySelector('#subject');
 const contactMessage = document.querySelector('#message');
+
 contactForm.addEventListener('submit', (event)=>{
     event.preventDefault();
-    const msg = {
-        contactName: contactName.value,
-        contactEmail: contactEmail.value,
-        contactSubject: contactSubject.value,
-        contactMessage: contactMessage.value
-    }
-    mail(msg)
-})
+    sendEmail();
+});
 
-const mail = (mail) =>{
-    fetch( `/mail?msg=${mail}` )
+function sendEmail() {
+    Email.send({
+    Host: 'smtp.sendgrid.net',
+    Username : 'apikey',
+    Password : process.env.SENDGRID_API_KEY,
+    to: contactEmail.value,
+    From: process.env.PERSONAL_MAIL, // Change to your verified sender
+    Subject: contactSubject.value,
+    text: contactMessage.value,
+    Body: `<strong>${contactName.value}</strong>`,
+    }).then(
+        message => alert("mail sent successfully")
+    );
 }
